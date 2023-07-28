94a32f4fe2c62c63a814ff7a912497b4d09aa33c
TownTowner <tanghelv@qq.com>
2023-07-29 00:22:27 +0800

    Add more serilog config.

--- Hotel/WebHostExtensions.cs
+++ Hotel/WebHostExtensions.cs
@@ -25,7 +25,20 @@ public static class WebHostExtensions
             {
                 AutoRegisterTemplate = true,
                 IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{env.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+                //,ModifyConnectionSettings = (conn) =>
+                //{
+                //    conn.ServerCertificateValidationCallback((source, cert, chain, error) => true);
+                //    conn.BasicAuthentication("elastic", "elastic");
+                //    return conn;
+                //}
+                //,EmitEventFailure = EmitEventFailureHandling.RaiseCallback
             };
+            //Entich.FromLogContext 这里的作用将一些环境信息写入日志中 
+            //new ElasticsearchSinkOptions设置了ES的地址； 
+            //IndexFormat 默认是到天（hotel-[development/production]-{0:yyyy.MM.dd}），我修改成到月。不同IndexFormat的日志会存储在不同索引中；
+            //EmitEventFailure 设置了当失败时调用FailureCallback
+            //ModifyConnectionSettings这里需要设置了2个地方，一个是忽略证书, 一个是设置了 BasicAuthentioncation，也就是账号密码 。若没有设置密码，则无需要配置。
+
             // Create Logger
             //Log.Logger = new LoggerConfiguration()
             loggingConfiguration
